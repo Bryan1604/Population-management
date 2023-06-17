bb8038014930ed674ca466fcc74cb194753ae553
trungduc1294 <73752537+trungduc1294@users.noreply.github.com>
2023-06-17 21:29:55 +0700

    sua lung tung dung merge vao

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="navigator">
         <h2 class="nav_menu__heading">QUẢN LÝ CÔNG DÂN</h2>
         <ul class="nav_menu_items">
-            <li><a href="#">Thống kê</a></li>
+            <li><a href="route(pages.dashboard)">Thống kê</a></li>
             <li class="item_actived"><a href="#">Thông tin hộ khẩu</a></li>
             <li><a href="#">Thông tin nhân khẩu</a></li>
             <li><a href="#">Tạm trú tạm vắng</a></li>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
     return view('layouts/main');
 });
 
-Route::get('/dashboard',[PeopleController::class,'calculateChart'])->name('pages/dashboard');
+Route::get('/dashboard',[PeopleController::class,'calculateChart'])->name('pages.dashboard');
 // Route::get('/dashboard', function () {
 //     return view('pages/dashboard');
 // });
