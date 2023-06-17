e7992009dba95e9beeb8a1cc85afe1e6217e9e9e
zyyx-nghiann <nghia.nn@zyyx.jp>
2023-06-17 16:55:23 +0700

    add residence form

--- resources/views/layouts/line_chart.blade.php
+++ resources/views/layouts/line_chart.blade.php
@@ -1,37 +1,37 @@
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
-google.charts.load('current', {
-    'packages': ['corechart']
-});
-google.charts.setOnLoadCallback(drawChart);
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Month', 'People'],
-        ['1', 1000],
-        ['2', 1170],
-        ['3', 997],
-        ['4', 1200],
-        ['5', 1190],
-        ['6', 1050],
-        ['7', 800],
-        ['8', 700],
-        ['9', 1000],
-        ['10', 1150],
-        ['11', 1200],
-        ['12', 1280],
-    ]);
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Month', 'People'],
+            ['1', '{{$lineChartData[0]}}' * 100 / 100],
+            ['2', '{{$lineChartData[1]}}' * 100 / 100],
+            ['3', '{{$lineChartData[2]}}' * 100 / 100],
+            ['4', '{{$lineChartData[3]}}' * 100 / 100],
+            ['5', '{{$lineChartData[4]}}' * 100 / 100],
+            ['6', '{{$lineChartData[5]}}' * 100 / 100],
+            ['7', '{{$lineChartData[6]}}' * 100 / 100],
+            ['8', '{{$lineChartData[7]}}' * 100 / 100],
+            ['9', '{{$lineChartData[8]}}' * 100 / 100],
+            ['10', '{{$lineChartData[9]}}' * 100 / 100],
+            ['11', '{{$lineChartData[10]}}' * 100 / 100],
+            ['12', '{{$lineChartData[11]}}' * 100 / 100],
+        ]);
 
-    var options = {
-        title: 'Biểu đồ biến đổi dân cư',
-        curveType: 'function',
-        legend: {
-            position: 'bottom'
-        }
-    };
+        var options = {
+            title: 'Biểu đồ biến đổi dân cư',
+            curveType: 'function',
+            legend: {
+                position: 'bottom'
+            }
+        };
 
-    var chart = new google.visualization.LineChart(document.getElementById('Curve_chart'));
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 
-    chart.draw(data, options);
-}
+        chart.draw(data, options);
+    }
 </script>
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -4,40 +4,7 @@
         <div class="chart_container_up">
             <div class="population_change_chart">
                 <div id="curve_chart" style="width: 445px; height: 300px"></div>
-                <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-                <script type="text/javascript">
-                    google.charts.load('current', {'packages':['corechart']});
-                    google.charts.setOnLoadCallback(drawChart);
-
-                    function drawChart() {
-                        var data = google.visualization.arrayToDataTable([
-                            ['Month', 'People'],
-                            ['1',  '{{$lineChartData[0]}}'*100/100],
-                            ['2',  '{{$lineChartData[1]}}'*100/100],
-                            ['3',  '{{$lineChartData[2]}}'*100/100],
-                            ['4',  '{{$lineChartData[3]}}'*100/100],
-                            ['5',  '{{$lineChartData[4]}}'*100/100],
-                            ['6',  '{{$lineChartData[5]}}'*100/100],
-                            ['7',  '{{$lineChartData[6]}}'*100/100],
-                            ['8', '{{$lineChartData[7]}}'*100/100],
-                            ['9',  '{{$lineChartData[8]}}'*100/100],
-                            ['10',  '{{$lineChartData[9]}}'*100/100],
-                            ['11',  '{{$lineChartData[10]}}'*100/100],
-                            ['12',  '{{$lineChartData[11]}}'*100/100],
-                        ]);
-
-                        var options = {
-                            title: 'Biểu đồ biến đổi dân cư',
-                            curveType: 'function',
-                            legend: { position: 'bottom' }
-                        };
-
-                        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
-                        chart.draw(data, options);
-                    }
-                </script>
-
+                @include('layouts/line_chart')
             </div>
             <div class="population_classification">
                 <div id="piechart" style="width: 445px; height: 300px;"></div>
